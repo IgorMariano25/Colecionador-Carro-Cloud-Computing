5f9bbd6c9e09c41709d46b6f4cb40b808f8ac07b
Igor Mariano <igor-mariano@outlook.com>
2023-09-16 17:59:10 -0300

    Modificando a Interface CarroRepository

--- src/main/java/br/com/colecionador/api_carros/repository/CarroRepsoitory.java
+++ src/main/java/br/com/colecionador/api_carros/repository/CarroRepsoitory.java
@@ -1,5 +1,10 @@
 package br.com.colecionador.api_carros.repository;
 
-public class CarroRepsoitory {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.com.colecionador.api_carros.model.Carro;
+
+@Repository
+public interface CarroRepsoitory extends JpaRepository<Carro, Long>{
     
 }
