2622a994e090da989f0ba703d09453b8b9a5650d
Igor Mariano <igor-mariano@outlook.com>
2023-09-16 17:59:20 -0300

    Modificando a Interface ColecionadorRepository

--- src/main/java/br/com/colecionador/api_carros/repository/ColecionadorRepository.java
+++ src/main/java/br/com/colecionador/api_carros/repository/ColecionadorRepository.java
@@ -1,5 +1,10 @@
 package br.com.colecionador.api_carros.repository;
 
-public class ColecionadorRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.com.colecionador.api_carros.model.Colecionador;
+
+@Repository
+public interface ColecionadorRepository extends JpaRepository<Colecionador, Long>{
     
 }
