c4769da71be1a30aa3e4a90b6c13c36b99531c07
Igor Mariano <igor-mariano@outlook.com>
2023-09-16 11:39:25 -0300

    Enviando arquivos padrões do projeto criado com SpringBoot

---
+++ src/test/java/br/com/colecionador/api_carros/ApiCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.colecionador.api_carros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
