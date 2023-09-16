9d7667c64c86d1f0ff02ac215a676aedee94bafa
Igor Mariano <igor-mariano@outlook.com>
2023-09-16 17:59:37 -0300

    Modificando a Interface EnderecoRepository

--- src/main/java/br/com/colecionador/api_carros/repository/EnderecoRepository.java
+++ src/main/java/br/com/colecionador/api_carros/repository/EnderecoRepository.java
@@ -1,5 +1,10 @@
 package br.com.colecionador.api_carros.repository;
 
-public class EnderecoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.com.colecionador.api_carros.model.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long>{
     
 }
