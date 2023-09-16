729e9dd1be92a56a3b234a173a09b0b01f1514e3
Igor Mariano <igor-mariano@outlook.com>
2023-09-16 18:11:11 -0300

    Criando atributos para a classe Carro

--- src/main/java/br/com/colecionador/api_carros/model/Carro.java
+++ src/main/java/br/com/colecionador/api_carros/model/Carro.java
@@ -1,5 +1,25 @@
 package br.com.colecionador.api_carros.model;
 
+
 public class Carro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     
+    @Column(nullable = false)
+    @NotBlank(message = "O campo marca não pode ser vazio")
+    private String marca;
+
+    @Column(nullable = false)
+    @NotBlank(message = "O campo modelo não pode ser vazio")
+    private String modelo;
+
+    @Column(nullable = true)
+    @NotBlank(message = "O campo ano de fabricação não pode ser vazio")
+    private String anoDeFabricacao;
+
+    @Column(nullable = true)
+    @NotBlank(message = "O campo quilometragem não pode ser vazio")
+    private Long quilometragem;
 }
