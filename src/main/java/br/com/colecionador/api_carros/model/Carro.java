8948e09034ff0ddfd65ff6e455de7b79032d043b
Igor Mariano <igor-mariano@outlook.com>
2023-09-16 18:11:28 -0300

    Adicionando imports

--- src/main/java/br/com/colecionador/api_carros/model/Carro.java
+++ src/main/java/br/com/colecionador/api_carros/model/Carro.java
@@ -1,5 +1,12 @@
 package br.com.colecionador.api_carros.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public class Carro {
 
