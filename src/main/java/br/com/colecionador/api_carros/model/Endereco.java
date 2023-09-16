c05df606cfdb92d7fa6e94d53e51b875618bbbbd
Igor Mariano <igor-mariano@outlook.com>
2023-09-16 17:01:53 -0300

    Adicionando @NotBlank em Endereco

--- src/main/java/br/com/colecionador/api_carros/model/Endereco.java
+++ src/main/java/br/com/colecionador/api_carros/model/Endereco.java
@@ -6,28 +6,34 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name= "enderecos")
 public class Endereco {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     
     @Column(length = 200, nullable = false)
+    @NotBlank(message = "O campo logradouro não pode ser vazio")
     private String logradouro;
 
     @Column(length = 200, nullable = false)
+    @NotBlank(message = "O campo complemento não pode ser vazio")
     private String complemento;
-
+    
     @Column(length = 200, nullable = false)
+    @NotBlank(message = "O campo cidade não pode ser vazio")
     private String cidade;
 
     @Column(length = 100, nullable = false)
+    @NotBlank(message = "O campo estado não pode ser vazio")
     private String estado;
     
     @Column(length = 100, nullable = false)
+    @NotBlank(message = "O campo cep não pode ser vazio")
     private String cep;
 
     public String getCep() {
