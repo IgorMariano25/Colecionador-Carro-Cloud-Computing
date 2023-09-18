05d90d6ece8615d30f8a66aefc40ba04c26d9559
Igor Mariano <igor-mariano@outlook.com>
2023-09-18 08:40:51 -0300

    Adicionando @Entity e @Table na classe Carro

--- src/main/java/br/com/colecionador/api_carros/model/Carro.java
+++ src/main/java/br/com/colecionador/api_carros/model/Carro.java
@@ -1,11 +1,15 @@
 package br.com.colecionador.api_carros.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 
+@Entity
+@Table(name = "carros")
 public class Carro {
 
     @Id
