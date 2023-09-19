7b3afd7e6f8edcf12f0cd8227ab7f64787d66c3d
Igor Mariano <igor-mariano@outlook.com>
2023-09-19 19:22:50 -0300

    Adicionando imports

--- src/main/java/br/com/colecionador/api_carros/controller/EnderecoController.java
+++ src/main/java/br/com/colecionador/api_carros/controller/EnderecoController.java
@@ -1,5 +1,25 @@
 package br.com.colecionador.api_carros.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.colecionador.api_carros.model.Endereco;
+import br.com.colecionador.api_carros.repository.EnderecoRepository;
+import jakarta.validation.Valid;
+
 public class EnderecoController {
     
 }
