1ba35a9e651920da635d518c7933691352e83fd7
Igor Mariano <igor-mariano@outlook.com>
2023-09-19 20:17:50 -0300

    Criando um método para adicionar Endereços ao colecionador

--- src/main/java/br/com/colecionador/api_carros/model/Colecionador.java
+++ src/main/java/br/com/colecionador/api_carros/model/Colecionador.java
@@ -120,4 +120,8 @@ public class Colecionador {
     public void setCarros(List<Carro> carros) {
         this.carros = carros;
     }
+
+    public void adicionarEndereco(Endereco endereco){
+        this.enderecos.add(endereco);
+    }
 }
