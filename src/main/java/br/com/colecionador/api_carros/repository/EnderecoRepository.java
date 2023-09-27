99fc9b70312d41fe9015824b609b2a2b9a5b45cb
Igor Mariano <igor-mariano@outlook.com>
2023-09-27 10:26:22 -0300

    Adicionando método findById

--- src/main/java/br/com/colecionador/api_carros/repository/EnderecoRepository.java
+++ src/main/java/br/com/colecionador/api_carros/repository/EnderecoRepository.java
@@ -1,10 +1,12 @@
 package br.com.colecionador.api_carros.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import br.com.colecionador.api_carros.model.Endereco;
 
 @Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long>{
-    
+   Optional<Endereco> findById(Long id);
 }
