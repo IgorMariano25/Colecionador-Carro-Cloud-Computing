8f70dc439b40369ded3e2015490a4b5726b23d3f
Igor Mariano <igor-mariano@outlook.com>
2023-09-27 10:56:08 -0300

    Adicionando @ControllerAdvice

--- src/main/java/br/com/colecionador/api_carros/errorHandler/ErrorHandlerController.java
+++ src/main/java/br/com/colecionador/api_carros/errorHandler/ErrorHandlerController.java
@@ -1,5 +1,8 @@
 package br.com.colecionador.api_carros.errorHandler;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
 public class ErrorHandlerController {
     
 }
