9871cfad0c65e7c367d3993ab24f26c1cb98d36e
Igor Mariano <igor-mariano@outlook.com>
2023-10-11 10:17:49 -0300

    Criando o método uploadFileToAzure

--- src/main/java/br/com/colecionador/api_carros/service/AzureStorageAccountService.java
+++ src/main/java/br/com/colecionador/api_carros/service/AzureStorageAccountService.java
@@ -10,5 +10,19 @@ import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobContainerClientBuilder;
 
 public class AzureStorageAccountService {
-    
+
+    public String uploadFileToAzure(MultipartFile file) throws IOException {
+        String connectionString = "DefaultEndpointsProtocol=https;AccountName=storageapicarros;AccountKey=tRGdeh7llYq0/m5AX7AwcuqTKbw2WuXOB5lcSwdONvDjex3pPmuQvoiaVohgOmhPp1MK9yvaQvJm+ASt5/3/HA==;EndpointSuffix=core.windows.net";
+
+        BlobContainerClient client = new BlobContainerClientBuilder()
+                .connectionString(connectionString)
+                .containerName("images")
+                .buildClient();
+
+        BlobClient blob = client.getBlobClient(file.getOriginalFilename());
+
+        blob.upload(file.getInputStream(), file.getSize(), true);
+
+        return "https://storageapicarros.blob.core.windows.net/images" + file.getOriginalFilename();
+    }
 }
