3a88eaaefb4725d7ffd874e20bd55033c892c418
Igor Mariano <igor-mariano@outlook.com>
2023-10-11 09:49:58 -0300

    Adicionando imports necessários

--- src/main/java/br/com/colecionador/api_carros/service/AzureStorageAccountService.java
+++ src/main/java/br/com/colecionador/api_carros/service/AzureStorageAccountService.java
@@ -1,5 +1,14 @@
 package br.com.colecionador.api_carros.service;
 
+import java.io.IOException;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobContainerClientBuilder;
+
 public class AzureStorageAccountService {
     
 }
