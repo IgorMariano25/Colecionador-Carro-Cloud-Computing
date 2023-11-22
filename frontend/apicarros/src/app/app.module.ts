fed21024a153f66c4eeb8bd4b7d39ac48cf4fc72
Igor Mariano <igor-mariano@outlook.com>
2023-11-22 15:03:09 -0300

    Adicionando novo import

--- frontend/apicarros/src/app/app.module.ts
+++ frontend/apicarros/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -24,7 +25,8 @@ import { TabelaColecionadoresComponent } from './componentes/tabela-colecionador
     MatIconModule,
     RouterModule,
     MatTableModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
