94464c93deeb6a03e5599ae985b85807265b7ff4
Igor Mariano <igor-mariano@outlook.com>
2023-11-23 11:07:45 -0300

    Adicionando CrossOrigin

--- src/main/java/br/com/colecionador/api_carros/controller/CarroController.java
+++ src/main/java/br/com/colecionador/api_carros/controller/CarroController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "*")
 @Tag(name = "Carro", description = "Resquições para a tabela Carro")
 @RequestMapping("/carro")
 public class CarroController {
--- src/main/java/br/com/colecionador/api_carros/controller/EnderecoController.java
+++ src/main/java/br/com/colecionador/api_carros/controller/EnderecoController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@CrossOrigin(origins = "*")
 @Tag(name = "Endereço", description = "Resquições para a tabela Endereco")
 @RequestMapping("/endereco")
 public class EnderecoController {
