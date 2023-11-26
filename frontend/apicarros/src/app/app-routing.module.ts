0c5d0444124601662ff0e79ec94374ce8864ad02
Igor Mariano <igor-mariano@outlook.com>
2023-11-26 18:02:12 -0300

    Adicionando novas rotas de páginas

--- frontend/apicarros/src/app/app-routing.module.ts
+++ frontend/apicarros/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllCarsComponent } from './pages/AllCars/all-cars.component'
+import { CarrosColecionadorComponent } from './pages/CarrosColecionador/carros-colecionador.component'
+import { HomeComponent } from './pages/Home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "allCars", component: AllCarsComponent },
+  { path: "carsCollector", component: CarrosColecionadorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
