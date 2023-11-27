774e12f732f5111d6267ceb22736289243443951
Igor Mariano <igor-mariano@outlook.com>
2023-11-27 20:12:47 -0300

    Removendo código TS desnecesário home

--- frontend/apicarros/src/app/pages/Home/home.component.ts
+++ frontend/apicarros/src/app/pages/Home/home.component.ts
@@ -14,23 +14,10 @@ export class HomeComponent implements OnInit {
   colecionador?: ColecionadorModel[];
   carro?: CarroModel[];
 
-  constructor(private colecionadorService: ColecionadorService, private carroService: CarroService, private router: Router, private route: ActivatedRoute) {
+  constructor() {
 
   }
 
   ngOnInit(): void {
-
-    let idColecionador = this.route.snapshot.params["id"];
-    this.colecionadorService.getColecionadorById(idColecionador).subscribe(response => {
-      this.colecionador = [response];
-    });
-
-    this.carroService.getCarrosByColecionadorId(idColecionador).subscribe(response => {
-      this.carro = response;
-    });
-  }
-
-  redirectTocarsCollector(idColecionador:any) {
-    this.router.navigate(["carsCollector", idColecionador]);
   }
 }
