ec6af9b3b10cc11c08a1e3637ff8a45abff32ca1
Igor Mariano <igor-mariano@outlook.com>
2023-11-27 20:11:25 -0300

    Modificando TS colecionadores

--- frontend/apicarros/src/app/pages/Colecionadores/colecionadores.component.ts
+++ frontend/apicarros/src/app/pages/Colecionadores/colecionadores.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ColecionadorService } from '../../services/colecionador/colecionador.service';
+import { Router } from '@angular/router';
+import { CarroService } from '../../services/carro/carro.service';
 
 @Component({
   selector: 'app-colecionadores',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './colecionadores.component.css'
 })
 export class ColecionadoresComponent {
+  carros: any;
+  colecionador: any;
 
+  constructor(
+    private colecionadorService: ColecionadorService,
+    private carroService: CarroService,
+    private router: Router) {
+  }
+
+  ngOnInit(): void {
+
+    this.colecionadorService?.findAll().subscribe(response => {
+      this.colecionador = response;
+    });
+  }
+  redirectTocarsCollector(id: any) {
+    if (id) {
+      this.carroService.findCarroByColecionadorId(id).subscribe(carros => {
+        this.carros = carros;
+        this.router.navigate(['/carsCollector', id]);
+      });
+    }
+  }
 }
