09512e092fbf31d1bcb0f82fbcfa4a0efeeeaef8
Igor Mariano <igor-mariano@outlook.com>
2023-12-23 09:20:00 -0300

    Criando funções para abrir e fechar o popup quando o colecionador for criado

--- frontend/src/app/pages/FormularioColecionador/formulario-colecionador.component.ts
+++ frontend/src/app/pages/FormularioColecionador/formulario-colecionador.component.ts
@@ -19,6 +19,21 @@ export class FormularioColecionadorComponent {
 
   constructor(private colecionadorService: ColecionadorService) { }
 
+  public openPopUp() {
+    let popup = document.querySelector(".popup");
+    popup?.classList.add("open__popup");
+
+    let popupBackgroundView = document.querySelector(".popup__view");
+    popupBackgroundView?.classList.add("active");
+  }
+
+  public closePopUp() {
+    let popup = document.querySelector(".popup");
+    let popupBackgroundView = document.querySelector(".popup__view");
+    popup?.classList.remove("open__popup");
+    popupBackgroundView?.classList.remove("active");
+  }
+
   public criarNovoColecionador() {
     const novoColecionador: ColecionadorModel = {
       nome: this.colecionador.nome as string,
