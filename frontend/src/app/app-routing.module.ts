d8fb3c4672b4827549b41f31ed49d475965d3b41
Igor Mariano <igor-mariano@outlook.com>
2023-12-27 15:03:07 -0300

    Adicionando novas rotas de navegação

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './pages/Home/home.component';
 import { ColecionadoresComponent } from './pages/Colecionadores/colecionadores.component';
 import { FormularioColecionadorComponent } from './pages/Formularios/FormularioColecionador/formulario-colecionador.component';
 import { FormularioCarroComponent } from './pages/Formularios/FormularioCarro/formulario-carro.component';
+import { AboutComponent } from './pages/AboutCompany/about.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
@@ -13,7 +14,9 @@ const routes: Routes = [
   { path: "carsCollector/:id", component: CarrosColecionadorComponent },
   { path: "Colecionadores", component: ColecionadoresComponent },
   { path: "FormularioColecionador", component: FormularioColecionadorComponent },
-  { path: "FormularioCarro", component: FormularioCarroComponent }
+  { path: "FormularioCarro", component: FormularioCarroComponent },
+  { path: "About/:textoUrl", component: AboutComponent },
+  { path: "About", component: AboutComponent }
 ];
 
 @NgModule({
