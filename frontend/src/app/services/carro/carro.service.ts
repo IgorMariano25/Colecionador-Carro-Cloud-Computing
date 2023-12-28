d6415823066633afc459978e32f6ea5612967381
Igor Mariano <igor-mariano@outlook.com>
2023-12-28 14:51:17 -0300

    Adicionando os métodos getCarrosByMarca e createCarro

--- frontend/src/app/services/carro/carro.service.ts
+++ frontend/src/app/services/carro/carro.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CarroModel } from '../../model/carro.model';
+import { ColecionadorModel } from './../../model/colecionador.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -17,7 +18,15 @@ export class CarroService {
   public getCarrosById(id: any): Observable<CarroModel> {
     return this.httpClient.get<CarroModel>(`https://deploy-ap1-cloud-computing-2023-2.azurewebsites.net/${id}`);
   }
+  public getCarrosByMarca(marca: any): Observable<CarroModel> {
+    return this.httpClient.get<CarroModel>(`https://deploy-ap1-cloud-computing-2023-2.azurewebsites.net/${marca}`);
+  }
+
   public findCarroByColecionadorId(id: any): Observable<CarroModel[]> {
     return this.httpClient.get<CarroModel[]>(`https://deploy-ap1-cloud-computing-2023-2.azurewebsites.net/carro/colecionador/${id}`);
+  }
+
+  public createCarro(colecionador: ColecionadorModel, carro: CarroModel): Observable<CarroModel> {
+    return this.httpClient.post<CarroModel>(`https://deploy-ap1-cloud-computing-2023-2.azurewebsites.net/colecionador/${colecionador.id}`, carro);
   }
 }
