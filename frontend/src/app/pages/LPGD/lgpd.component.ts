6c0b6873742fd1f0b008ad4fbc1466d3a4e70ffb
Igor Mariano <igor-mariano@outlook.com>
2024-01-06 20:56:12 -0300

    Criando arquivos padrões da página de LGPD

---
+++ frontend/src/app/pages/LPGD/lgpd.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lgpd',
+  templateUrl: './lgpd.component.html',
+  styleUrl: './lgpd.component.css'
+})
+export class LgpdComponent {
+
+}
