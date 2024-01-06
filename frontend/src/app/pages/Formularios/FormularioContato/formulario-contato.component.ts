6c5a61fac76f9ba46ca3bb95bb0d702a8fa9296a
Igor Mariano <igor-mariano@outlook.com>
2024-01-06 19:19:17 -0300

    Adicionando função para abrir e fechar o pop-up

--- frontend/src/app/pages/Formularios/FormularioContato/formulario-contato.component.ts
+++ frontend/src/app/pages/Formularios/FormularioContato/formulario-contato.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './formulario-contato.component.css'
 })
 export class FormularioContatoComponent {
+  public openPopUp() {
+    let popup = document.querySelector(".popup");
+    popup?.classList.add("open__popup");
 
+    let popupBackgroundView = document.querySelector(".popup__view");
+    popupBackgroundView?.classList.add("active");
+  }
+
+  public closePopUp() {
+    let popup = document.querySelector(".popup");
+    let popupBackgroundView = document.querySelector(".popup__view");
+    popup?.classList.remove("open__popup");
+    popupBackgroundView?.classList.remove("active");
+  }
 }
