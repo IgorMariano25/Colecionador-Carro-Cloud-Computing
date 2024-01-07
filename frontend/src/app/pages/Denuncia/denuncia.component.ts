73217dc8e662b734cf37b60071af52df3b847447
Igor Mariano <igor-mariano@outlook.com>
2024-01-07 10:06:54 -0300

    Criando arquivos básicos da página de Denuncia

---
+++ frontend/src/app/pages/Denuncia/denuncia.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-denuncia',
+  templateUrl: './denuncia.component.html',
+  styleUrl: './denuncia.component.css'
+})
+export class DenunciaComponent {
+
+}
