fd91cc8e5c6f1095cc5c4b0e7f553c0fe82143d0
Igor Mariano <igor-mariano@outlook.com>
2024-01-07 09:50:59 -0300

    Criando arquivos básicos da página de imprensa

---
+++ frontend/src/app/pages/Imprensa/imprensa.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imprensa',
+  templateUrl: './imprensa.component.html',
+  styleUrl: './imprensa.component.css'
+})
+export class ImprensaComponent {
+
+}
