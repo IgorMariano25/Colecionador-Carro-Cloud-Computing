05b372411f72302c0edafaea5999265f860968d1
Igor Mariano <igor-mariano@outlook.com>
2024-01-09 14:08:00 -0300

    Criando arquivos padrões da página de subsidiária

---
+++ frontend/src/app/pages/subsidiaria/subsidiaria.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subsidiaria',
+  templateUrl: './subsidiaria.component.html',
+  styleUrl: './subsidiaria.component.css'
+})
+export class SubsidiariaComponent {
+
+}
