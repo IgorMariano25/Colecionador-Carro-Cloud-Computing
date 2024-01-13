799d5183e51c8e5964552679790d6a66112ad415
Igor Mariano <igor-mariano@outlook.com>
2024-01-13 16:05:56 -0300

    Adicionando sooth scroll para elementos com base no id

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,24 +1,71 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { VisibilityService } from './services/VisibilityService/visibility.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor(private router: Router) { }
 
-  pageUrl(urlPath: string) {
-    this.router.navigate(['/About', urlPath]);
+export class AppComponent implements OnInit {
+  // @ViewChild('navbar') header!: ElementRef;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    public visibilityService: VisibilityService) { }
+
+  ngOnInit() {
+    this.route.fragment.subscribe((fragment: string | null) => {
+      if (fragment) {
+        this.scrollToElement(fragment);
+      }
+    });
   }
 
-  redirectToAboutPage(id: string) {
-    const navigateToElement = document.getElementById(id);
+  redirectToAboutPage(fragment: string) {
+    this.router.navigate(['/About'], { fragment: fragment });
+  }
 
-    if (navigateToElement) {
-      navigateToElement.addEventListener("click", () => this.pageUrl(id));
-    }
+  smoothScrollTo(endX: number, endY: number, duration: number): void {
+    const startX: number = window.scrollX;
+    const startY: number = window.scrollY;
+    const distanceX: number = endX - startX;
+    const distanceY: number = endY - startY;
+    const startTime: number = new Date().getTime();
+
+    duration = typeof duration !== "undefined" ? duration : 400;
+
+    const easeInOutQuart = (time: number, from: number, distance: number, duration: number): number => {
+      if ((time /= duration / 2) < 1)
+        return (distance / 2) * time * time * time * time + from;
+      return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
+    };
+
+    const timer = setInterval(() => {
+      const time: number = new Date().getTime() - startTime;
+      const newX: number = easeInOutQuart(time, startX, distanceX, duration);
+      const newY: number = easeInOutQuart(time, startY, distanceY, duration);
+      if (time >= duration) {
+        clearInterval(timer);
+      }
+      window.scroll(newX, newY);
+    }, 1000 / 60);
+  }
+
+  private scrollToElement(id: string) {
+    const offsetTop = 85;
+
+    setTimeout(() => {
+      const element = document.getElementById(id);
+      if (element) {
+        const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+        const offsetPosition = elementPosition - offsetTop;
+
+        this.smoothScrollTo(0, offsetPosition, 3000);
+      }
+    });
   }
 }
 
