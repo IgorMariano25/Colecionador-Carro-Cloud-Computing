651ac00429b990d22336e7ff4c0079ee66db20d2
Igor Mariano <igor-mariano@outlook.com>
2024-01-15 15:10:33 -0300

    Criando componentes básicos da página de Estudantes

---
+++ frontend/src/app/pages/EmpregosECarreias/Estudantes/estudantes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estudantes',
+  templateUrl: './estudantes.component.html',
+  styleUrl: './estudantes.component.css'
+})
+export class EstudantesComponent {
+
+}
