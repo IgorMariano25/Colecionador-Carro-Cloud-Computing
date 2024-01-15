7168f7f319ac9747cd53b7a4ffee7e78c0f2aeb7
Igor Mariano <igor-mariano@outlook.com>
2024-01-15 14:57:44 -0300

    Criando componentes básicos da página de candidatura

---
+++ frontend/src/app/pages/EmpregosECarreias/Estudantes/Candidatura/candidatura.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidatura',
+  templateUrl: './candidatura.component.html',
+  styleUrl: './candidatura.component.css'
+})
+export class CandidaturaComponent {
+
+}
