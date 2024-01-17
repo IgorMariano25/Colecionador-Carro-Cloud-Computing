374bbc3e0b4ebcdcf4040ef7cf81f519082b01d8
Igor Mariano <igor-mariano@outlook.com>
2024-01-17 11:24:37 -0300

    Refatorando fomruário de contato para herdar a classe Fomulários

--- frontend/src/app/pages/Formularios/FormularioContato/formulario-contato.component.ts
+++ frontend/src/app/pages/Formularios/FormularioContato/formulario-contato.component.ts
@@ -1,23 +1,10 @@
 import { Component } from '@angular/core';
+import { Formularios } from '../formularios';
 
 @Component({
   selector: 'app-formulario-contato',
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
 })
-export class FormularioContatoComponent {
-  public openPopUp() {
-    let popup = document.querySelector(".popup");
-    popup?.classList.add("open__popup");
-
-    let popupBackgroundView = document.querySelector(".popup__view");
-    popupBackgroundView?.classList.add("active");
-  }
-
-  public closePopUp() {
-    let popup = document.querySelector(".popup");
-    let popupBackgroundView = document.querySelector(".popup__view");
-    popup?.classList.remove("open__popup");
-    popupBackgroundView?.classList.remove("active");
-  }
+export class FormularioContatoComponent extends Formularios{
 }
