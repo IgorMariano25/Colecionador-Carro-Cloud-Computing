6408175089e0cba2ef523a45ca022aca429a916f
Igor Mariano <igor-mariano@outlook.com>
2024-01-17 11:23:29 -0300

    Criando funções TS gerais para todos os formulários

---
+++ frontend/src/app/pages/Formularios/formularios.ts
@@ -0,0 +1,16 @@
+export class Formularios {
+  public openPopUp() {
+    let popup = document.querySelector(".popup");
+    popup?.classList.add("open__popup");
+
+    let popupBackgroundView = document.querySelector(".popup__view");
+    popupBackgroundView?.classList.add("active");
+  }
+
+  public closePopUp() {
+    let popup = document.querySelector(".popup");
+    let popupBackgroundView = document.querySelector(".popup__view");
+    popup?.classList.remove("open__popup");
+    popupBackgroundView?.classList.remove("active");
+  }
+}
