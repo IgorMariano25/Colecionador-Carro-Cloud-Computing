38fe777d5c0dc09f357a1277ec640e66a08b22bf
Igor Mariano <igor-mariano@outlook.com>
2024-01-23 17:57:57 -0300

    Extendendo classe AppComponent para reutilzar suas funções

--- frontend/src/app/pages/EmpregosECarreias/empregos-ecarreiras.component.ts
+++ frontend/src/app/pages/EmpregosECarreias/empregos-ecarreiras.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-empregos-ecarreiras',
   templateUrl: './empregos-ecarreiras.component.html',
   styleUrl: './empregos-ecarreiras.component.css'
 })
-export class EmpregosECarreirasComponent {
+
+export class EmpregosECarreirasComponent extends AppComponent implements OnInit {
 
 }
