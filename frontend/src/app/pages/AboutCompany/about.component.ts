7cd355e4df639fcea6216a3602404daa2e688078
Igor Mariano <igor-mariano@outlook.com>
2024-01-28 19:46:57 -0300

    Extendendo a classe AppComponent

--- frontend/src/app/pages/AboutCompany/about.component.ts
+++ frontend/src/app/pages/AboutCompany/about.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
+import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
-
-
-}
+export class AboutComponent extends AppComponent { }
